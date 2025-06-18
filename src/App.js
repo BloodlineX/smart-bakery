67bdcf340d527863a99f7c54d675bd7b4e3f2447
BloodlineX <bhaskarc612@gmail.com>
2025-06-18 11:06:35 +0530

    Updated menu section images and size of images

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ return ( <div className="font-sans"> <header className="bg-yellow-100 shadow"> <
           items: ["Chocolate", "Blueberry", "Banana", "Vanilla"],
         },
         {
-          src: `${process.env.PUBLIC_URL}/cupcakes.jpg`,
+          src: `${process.env.PUBLIC_URL}/cupcake.jpg`,
           title: "Cupcakes",
           items: ["vanilla", "Chocolate","Cream filled", ],
         },
